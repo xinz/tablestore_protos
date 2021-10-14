6f01013cb1451fd71d73cecec49a114ad6758248
xinz <joy2everyone.z@gmail.com>
2021-10-14 14:39:41 +0800

    Version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TablestoreProtos.MixProject do
   def project do
     [
       app: :tablestore_protos,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
